48596af9caa4509e205af153ee46f712f172d1d8
Amos <amoshuke@qq.com>
2022-06-01 00:40:08 +0800

    fix: 调整路由参数访问方式、新增WebView

--- lib/routes.dart
+++ lib/routes.dart
@@ -17,28 +17,37 @@ import 'package:moodexample/views/mood/mood_category_select.dart';
 import 'package:moodexample/views/mood/mood_content.dart';
 import 'package:moodexample/views/settings/laboratory/index.dart';
 import 'package:moodexample/views/settings/laboratory/unimp_miniapps/index.dart';
+import 'package:moodexample/views/web_view/web_view.dart';
 
 /// 路由管理
 class Routes {
   /// 定义路由名称
-  // 主页
+  /// 主页
   static String home = "/";
-  // 心情详情列表页
+
+  /// 心情详情列表页
   static String mood = "/mood";
-  // 添加心情页
+
+  /// 添加心情页
   static String moodCategorySelect = "/mood_category_select";
-  // 添加心情内容页
+
+  /// 添加心情内容页
   static String moodContent = "/mood_content";
-  // 用户引导页
+
+  /// 用户引导页
   static String onboarding = "/onboarding";
 
-  // 设置页-实验室
+  /// 设置页-实验室
   static String settingLaboratory = "/setting_laboratory";
-  // 实验室-UniMP小程序
+
+  /// 实验室-UniMP小程序
   static String settingLaboratoryUniMPMiniapps =
       "/setting_laboratory_UniMPMiniapps";
 
-  /// 定义路由处理函数
+  /// WebView
+  static String webViewPage = "/web_view_page";
+
+  // 定义路由处理函数
   static final Handler _homeHandle = Handler(
       handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
     return const WillPopScopeRoute(child: HomePage());
@@ -68,6 +77,10 @@ class Routes {
       handlerFunc: (BuildContext? context, Map<String, List<String>> params) {
     return const UniMPMiniappsPage();
   });
+  static final Handler _webViewPage = Handler(
+      handlerFunc: (BuildContext? context, Map<String, dynamic> params) {
+    return WebViewPage(url: params["url"][0]);
+  });
 
   /// 关联路由名称和处理函数
   static void configureRoutes(FluroRouter router) {
@@ -91,16 +104,24 @@ class Routes {
     router.define(settingLaboratoryUniMPMiniapps,
         handler: _settingLaboratoryUniMPMiniappsHandle,
         transitionType: TransitionType.cupertino);
+    router.define("$webViewPage/:url",
+        handler: _webViewPage, transitionType: TransitionType.fadeIn);
   }
 
-  /// 路由参数转换
+  /// 路由带参数
+  ///
   /// 在路由需要传输参数时，将参数一一对应传入，返回String
-  /// 例如：transformParams(["1","2"]) => /1/2
-  static String transformParams(List<dynamic> params) {
+  ///
+  /// 例如：transformParams(router,["1","2"]) => router/1/2
+  static String transformParams({
+    required String router,
+    required List<dynamic> params,
+  }) {
     late String transform = "";
     for (int i = 0; i < params.length; i++) {
       transform += "/${params[i]}";
     }
-    return transform;
+    debugPrint("$router$transform");
+    return "$router$transform";
   }
 }
