025317697d2c9925c2eee84089e5ab6859735280
Amos <amoshuke@qq.com>
2022-07-06 01:12:53 +0800

    feat: 适配 UniMPSDK_iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,18 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let uniMPMiniApps = FlutterMethodChannel(name: "UniMP_mini_apps",
+                                             binaryMessenger: controller.binaryMessenger)
+    uniMPMiniApps.setMethodCallHandler({
+        [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+        print(call.arguments!)
+        guard call.method == "open" else {
+          result(FlutterMethodNotImplemented)
+          return
+        }
+        print("233")
+    })
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
