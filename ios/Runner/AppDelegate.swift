5cbcaed19ecceb59112a0fb167b9749ba4c3768a
Amos <amoshuke@qq.com>
2022-07-07 00:56:49 +0800

    feat: iOS 小程序通道适配

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import UIKit
 import Flutter
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate,DCUniMPSDKEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -11,15 +11,72 @@ import Flutter
     let uniMPMiniApps = FlutterMethodChannel(name: "UniMP_mini_apps",
                                              binaryMessenger: controller.binaryMessenger)
     uniMPMiniApps.setMethodCallHandler({
-        [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
-        print(call.arguments!)
-        guard call.method == "open" else {
+      [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+      switch(call.method) {
+        case "open":
+          if let arguments = call.arguments as? Dictionary<String,Any> {
+            let AppID: String = arguments["AppID"] as? String ?? ""
+            let options = NSMutableDictionary.init(dictionary: launchOptions ?? [:])
+            options.setValue(NSNumber.init(value:true), forKey: "debug")
+            DCUniMPSDKEngine.initSDKEnvironment(launchOptions: options as! [AnyHashable : Any]);
+            self?.checkUniMPResoutce(appid:AppID)
+            self?.openUniMP(appid:AppID)
+          }
+        break
+        default:
           result(FlutterMethodNotImplemented)
-          return
-        }
-        print("233")
+        break
+      }
     })
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func checkUniMPResoutce(appid: String) -> Void {
+    let wgtPath = Bundle.main.path(forResource: appid, ofType: "wgt") ?? ""
+    if DCUniMPSDKEngine.isExistsUniMP(appid) {
+      let version = DCUniMPSDKEngine.getUniMPVersionInfo(withAppid: appid)!
+      let name = version["code"]!
+      let code = version["code"]!
+      print("小程序：\(appid) 资源已存在，版本信息：name:\(name) code:\(code)")
+    } else {
+      do {
+        try DCUniMPSDKEngine.installUniMPResource(withAppid: appid, resourceFilePath: wgtPath, password: nil)
+        let version = DCUniMPSDKEngine.getUniMPVersionInfo(withAppid: appid)!
+        let name = version["code"]!
+        let code = version["code"]!
+        print("✅ 小程序：\(appid) 资源释放成功，版本信息：name:\(name) code:\(code)")
+      } catch let err as NSError {
+        print("❌ 小程序：\(appid) 资源释放失败:\(err)")
+      }
+    }
+  }
+
+  /// 打开uni小程序
+  @IBAction func openUniMP(appid: String) {
+    let configuration = DCUniMPConfiguration.init()
+    configuration.enableBackground = true
+    
+    DCUniMPSDKEngine.openUniMP(appid, configuration: configuration) { instance, error in
+        if instance != nil {
+            print("小程序打开成功")
+        } else {
+            print(error as Any)
+        }
+    }
+  }
+
+  // MARK: - DCUniMPSDKEngineDelegate
+  func uniMP(onClose appid: String) {
+    print("小程序：\(appid) closed")
+  }
+
+  func defaultMenuItemClicked(_ appid: String, identifier: String) {
+    print("defaultMenuItemClicked：\(appid) \(identifier)")
+  }
+
+  func splashView(forApp appid: String) -> UIView {
+      let splashView:UIView = Bundle.main.loadNibNamed("SplashView", owner: self, options: nil)?.last as! UIView
+      return splashView
   }
 }
