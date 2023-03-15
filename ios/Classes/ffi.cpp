f11e8d2d23e1f04ec65b484a7524b0775991da76
AmosHuKe <amoshuke@qq.com>
2023-03-15 15:13:03 +0800

    feat: 防止链接器在优化链接时会丢弃符号

--- ios/Classes/ffi.cpp
+++ ios/Classes/ffi.cpp
@@ -5,8 +5,10 @@
 #include "include/dart_native_api/dart_native_api.h"
 #include "include/dart_native_api/dart_api_dl.h"
 
+#define DART_API extern "C" __attribute__((visibility("default"))) __attribute__((used))
+
 // 声明执行函数
-void test(Dart_Port sendPort, int seconds);
+DART_API void test(Dart_Port sendPort, int seconds);
 
 // 初始化 Dart Native API
 // Initialize `dart_api_dl.h`
@@ -21,7 +23,7 @@ DART_EXPORT void RegisterSendPort(Dart_Port sendPort, int seconds) {
 }
 
 // 实际操作
-void test(Dart_Port sendPort, int seconds) {
+DART_API void test(Dart_Port sendPort, int seconds) {
   // 等待设定的时间
   std::this_thread::sleep_for(std::chrono::seconds(seconds));
   std::string text("这是线程 " + std::to_string(sendPort) +"\n设定 " + std::to_string(seconds) + " 秒后的消息");
